c73ca9511281592de0567d608d417990d88eac09
Antony Grachev <personalsmile69@gmail.com>
2025-04-15 08:09:57 +0300

    Add user optimizations and final sh Change project structure and info files

--- Postinstall_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo ./cmd/keyring_p0.sh && sudo ./cmd/main_p1.sh && ./cmd/yay_p2.sh && sudo ./cmd/sys_optimize_p3.sh #&& sudo ./Arch_Linux_Postinstall_fin.sh
-
---
+++ cmd/main/Postinstall_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo ./cmd/child/keyring_p0.sh && sudo ./cmd/child/main_p1.sh && ./cmd/child/yay_p2.sh && sudo ./cmd/child/sys_optimize_p3.sh
